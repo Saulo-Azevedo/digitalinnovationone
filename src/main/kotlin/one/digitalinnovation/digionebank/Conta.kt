c3c76e423037b0905ebb8cd6a4222fb7cb4b0385
Saulo-Azevedo <teksauloazevedo@gmail.com>
2021-03-16 23:46:44 -0300

    Created account class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+// para criar colocar () depois do conta e digitar enter !!!
+// Conta() e enter
+class Conta(
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+) {
+    fun deposito(valor: BigDecimal){
+
+    }
+    fun saque(valor: BigDecimal){
+
+    }
+}

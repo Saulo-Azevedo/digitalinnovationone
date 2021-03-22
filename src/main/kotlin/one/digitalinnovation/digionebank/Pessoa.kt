ae7a6ff664ff1484a5bddb7926e65a7bcabe69ee
Saulo-Azevedo <teksauloazevedo@gmail.com>
2021-03-21 23:08:36 -0300

    Created class Funcionario ( open using val)

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,20 +1,9 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "jether"
-    var cpf: String = "123.456.789-11"
-    private set
+// usar open para abrir a classe, permite que classe extenda e funcionario herde
+open class Pessoa(
+    open val nome: String,
+    open val cpf: String
+)
 
-    constructor()
-
-    fun pessoainfo() = "$nome e $cpf"
-}
-
-fun main(){
-    val jether = Pessoa()
-
-    println(jether.nome)
-    println(jether.cpf)
-    println(jether.pessoainfo())
-}
 
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Pessoa
+
+fun main(){
+    val jether = Pessoa(nome = "Jetther Rodrigues", cpf = "123.456.789-11")
+    println(jether.nome)
+    println(jether.cpf)
+
+}

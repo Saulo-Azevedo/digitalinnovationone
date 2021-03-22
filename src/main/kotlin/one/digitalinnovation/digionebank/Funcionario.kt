2ad231f59082298fafec7ebac1e0f612b789c4b0
Saulo-Azevedo <teksauloazevedo@gmail.com>
2021-03-21 23:18:11 -0300

    Created  commit for inactivate class TestPessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -1,10 +1,19 @@
 package one.digitalinnovation.digionebank
 
 import java.math.BigDecimal
-
+// usado quando temos val declarado na classe pessoa
+/*
 class Funcionario(
     override val nome: String,
     override val cpf: String,
+    val salario: BigDecimal
+):Pessoa(nome, cpf) {
+
+}*/
+
+class Funcionario(
+    nome: String,
+    cpf: String,
     val salario: BigDecimal
 ):Pessoa(nome, cpf) {
 
